969b5e25948ff6f88f0776e52b1f02c68c3bdef1
Pratap Lakshman <pratapl@microsoft.com>
2018-08-06 16:13:22 +0530

    Added sample for missing Override annotation.

---
+++ src/main/java/Advisory/Declarations/Child.java
@@ -0,0 +1,12 @@
+
+public class Child extends Parent
+{
+    // The @Override annotation serves as a reminder that the intent of the method is to override a parent method.
+
+    // @Override
+    public void m1()
+    {
+        System.out.println("From child m1()");
+    }
+     
+}
