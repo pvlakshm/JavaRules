d10f05e54834f266da132319d6d4cd040f7c19eb
Pratap Lakshman <pvlakshm@hotmail.com>
2018-08-09 16:22:04 +0530

    Minor edit to SelfAssignment sample.

--- src/main/java/LikelyBugs/LikelyTypos/SelfAssignment.java
+++ src/main/java/LikelyBugs/LikelyTypos/SelfAssignment.java
@@ -2,7 +2,7 @@ public class SelfAssignment
 {
     private int i;
 
-    public void TestableInt(int i)
+    public SelfAssignment(int i)
     {
         i = i;  // actually, the user meant this.i = i; so this is likely a typo.
     }
