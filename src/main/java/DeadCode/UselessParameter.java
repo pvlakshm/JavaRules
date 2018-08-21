ac5a8d24f4ba4d5e06bc2d035f960450c4b6ae77
Pratap Lakshman <pratapl@microsoft.com>
2018-08-21 22:45:07 +0530

    Added ref to rule.

--- src/main/java/DeadCode/UselessParameter.java
+++ src/main/java/DeadCode/UselessParameter.java
@@ -1,10 +1,20 @@
 public class UselessParameter
 {
+    // The param 'z' is not used and is useless
+    // While this is a contrived example, it is possible to end up with useless params in the course of refactoring.
+    //
+    // References:
+    // Rule: https://lgtm.com/rules/1506317566749/
+
     private boolean compare(int x, int y, int z)
     {
-        // The param 'z' is not used and is useless
-        // While this is a contrived example, it is possible to end up with useless params in the course of refactoring.
-        if (x < y) return true;
-        else return false;
+        if (x < y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
