ada9dddc2a4959b6b002ba2e57ee80159470a6f7
Pratap Lakshman <pratapl@microsoft.com>
2018-08-24 19:46:44 +0530

    Added sample for NullMaybe

---
+++ src/main/java/LikelyBugs/Nullness/NullMaybe.java
@@ -0,0 +1,11 @@
+public class NullMaybe
+{
+    public int getVal(String s)
+    {
+        // References:
+        // Rule doc: 
+        // QL query: https://github.com/lgtmhq/lgtm-queries/blob/master/java/Likely%20Bugs/Nullness/NullMaybe.ql
+
+        return s.length();
+    }
+}
