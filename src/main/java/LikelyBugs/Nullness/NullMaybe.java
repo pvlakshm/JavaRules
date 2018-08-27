3205bea97fee7395644df0c28157f80fe0922ec5
Pratap Lakshman <pratapl@microsoft.com>
2018-08-27 18:59:00 +0530

    NullMaybe with Integer.

--- src/main/java/LikelyBugs/Nullness/NullMaybe.java
+++ src/main/java/LikelyBugs/Nullness/NullMaybe.java
@@ -1,11 +1,11 @@
 public class NullMaybe
 {
-    public int getVal(String s)
+    public Integer getVal(Integer i)
     {
         // References:
         // Rule doc: 
         // QL query: https://github.com/lgtmhq/lgtm-queries/blob/master/java/Likely%20Bugs/Nullness/NullMaybe.ql
 
-        return true ? s.length() : 0;
+        return true ? i : 0;
     }
 }
