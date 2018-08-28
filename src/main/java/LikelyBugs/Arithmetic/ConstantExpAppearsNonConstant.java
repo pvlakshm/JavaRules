9c67fd920f08e82c3b30465734b007283bd11553
Pratap Lakshman <pratapl@microsoft.com>
2018-08-28 17:39:18 +0530

    Minor cleanup.

--- src/main/java/LikelyBugs/Arithmetic/ConstantExpAppearsNonConstant.java
+++ src/main/java/LikelyBugs/Arithmetic/ConstantExpAppearsNonConstant.java
@@ -8,12 +8,16 @@ public class ConstantExpAppearsNonConstant
         // QL query: https://github.com/lgtmhq/lgtm-queries/blob/master/java/Likely%20Bugs/Arithmetic/ConstantExpAppearsNonConstant.ql
 
         int iRes = i * 0;           // always evaluates to 0.
+
+/*
+        // several other examples:
         iRes = i % 1;               // always evaluates to 0.
         iRes = i << 32;             // always evaluates to 0;
         iRes = i & 0;               // always evaluates to 0. 
 
         long lRes = l << 64;        // always evaluates to 0.
         boolean bRes = b || true;   // always evaluates to true. 
-        bRes = b && false;          // always evaluates to false. 
+        bRes = b && false;          // always evaluates to false.
+*/
     }
 }
