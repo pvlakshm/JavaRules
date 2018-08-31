75955620697dddad6eecaa6d5d1aa9ea1dbd92e1
Pratap Lakshman <pvlakshm@hotmail.com>
2018-08-31 21:58:19 +0530

    Null maybe?

--- src/main/java/LikelyBugs/Nullness/NullMaybe.java
+++ src/main/java/LikelyBugs/Nullness/NullMaybe.java
@@ -1,11 +1,11 @@
 public class NullMaybe
 {
-    public Integer getVal(Integer i)
+    public int getVal(Integer i)
     {
         // References:
         // Rule doc: 
         // QL query: https://github.com/lgtmhq/lgtm-queries/blob/master/java/Likely%20Bugs/Nullness/NullMaybe.ql
 
-        return true ? i : 0;
+        return false? 0 : i;
     }
 }
