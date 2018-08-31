52e16756f1bfcde2369bca398b3c9a0a07ae8049
Pratap Lakshman <pvlakshm@hotmail.com>
2018-08-31 22:07:49 +0530

    Edit to null maybe.

--- src/main/java/LikelyBugs/Nullness/NullMaybe.java
+++ src/main/java/LikelyBugs/Nullness/NullMaybe.java
@@ -1,11 +1,16 @@
 public class NullMaybe
 {
-    public int getVal(Integer i)
+    public int getVal(Integer i, boolean b)
     {
         // References:
         // Rule doc: 
         // QL query: https://github.com/lgtmhq/lgtm-queries/blob/master/java/Likely%20Bugs/Nullness/NullMaybe.ql
 
-        return false? 0 : i;
+        if (b == true)
+        {
+            i = 5;
+        }
+
+        return i.intValue();
     }
 }
