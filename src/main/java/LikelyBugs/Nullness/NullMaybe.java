b9b09607c5ce5b985524194a2e1969eb1b76ae35
Pratap Lakshman <pratapl@microsoft.com>
2018-09-03 19:27:57 +0530

    Null maybe sample.

--- src/main/java/LikelyBugs/Nullness/NullMaybe.java
+++ src/main/java/LikelyBugs/Nullness/NullMaybe.java
@@ -1,16 +1,11 @@
 public class NullMaybe
 {
-    public int getVal(Integer i, boolean b)
+    public int getVal(Integer i)
     {
         // References:
-        // Rule doc: 
+        // Rule doc: https://lgtm.com/rules/1954750296/
         // QL query: https://github.com/lgtmhq/lgtm-queries/blob/master/java/Likely%20Bugs/Nullness/NullMaybe.ql
 
-        if (b == true)
-        {
-            i = 5;
-        }
-
-        return i.intValue();
+        return true ? i : 5;
     }
 }
